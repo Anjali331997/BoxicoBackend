40bbf10f2b408c084c79f9257bb17b4f971c8923
Anjali331997 <anjaliap3march1997@gmail.com>
2024-05-31 13:18:38 +0530

    editing env file link update

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-require('dotenv').config(); // Load environment variables from .env file
-
+require('dotenv').config();
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-// Enable CORS
+const PORT = process.env.PORT||8080;
 app.use(cors());
 
-// Endpoint to fetch data
-app.get('/api/sample-data', async (req, res) => {
+app.get('/sample-data', async (req, res) => {
   try {
-    // Make a GET request to the external API using the environment variable
-    const response = await axios.get(process.env.API_URL);
-    // Send the response data back to the client
+    const response = await axios.get(process.env.API_URL);t
     console.log(response);
     res.json(response.data);
   } catch (error) {
-    console.error('Error fetching data:', error);
-    // Send an error response if fetching data fails
-    res.status(500).json({ error: 'Failed to fetch data' });
+    res.status(500).json({ message:"Error while fetching data" });
   }
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
