c9f79df7335c52b86979dcd8325057efbf9d8489
Anjali331997 <anjaliap3march1997@gmail.com>
2024-05-31 13:28:22 +0530

    resolved error-end point was not working

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 require('dotenv').config();
+
 const app = express();
-const PORT = process.env.PORT||8080;
+const PORT = process.env.PORT || 3001;
+
 app.use(cors());
 
-app.get('/sample-data', async (req, res) => {
+app.get('/boxigo/sample-data', async (req, res) => {
   try {
-    const response = await axios.get(process.env.API_URL);t
+    const response = await axios.get(process.env.API_URL);
     console.log(response);
     res.json(response.data);
   } catch (error) {
-    res.status(500).json({ message:"Error while fetching data" });
+    res.status(500).json({ error: 'Error while fetching data' });
   }
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
